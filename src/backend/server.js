2536e6cb4eb09b61257fb2d8cd0f35b60e879bde
HoangTrung1010 <20120609@student.hcmus.edu.vn>
2023-04-21 15:14:06 +0700

    create backend project structure add user model for database mongodb

---
+++ src/backend/server.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+// const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:3002",
+    ],
+  })
+);
+
+// Make request using body x-www
+app.use("/api/users", require("./routes/authRoute"));
+app.get('/', (req, res) =>{
+    console.log("hi")
+})
+app.listen(port, () => console.log(`Server started on port ${port}`));
